45025b81a1948b6d072c4c463b8d2a3c6522a0cc
Alberto Biancardi <fieldyn@gmail.com>
2021-05-02 16:35:25 -0500

    42

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,8 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Core.Interfaces;
 using Core.Specifications;
+using API.Dtos;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -24,17 +26,37 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
     {
       var spec = new ProductsWithTypesAndBrandsSpecification();
       var products = await _productRepo.ListAsync(spec);
-      return Ok(products);
+      return products.Select(product => new ProductToReturnDto
+      {
+        Id = product.Id,
+        Name = product.Name,
+        Description = product.Description,
+        PictureUrl = product.PictureUrl,
+        Price = product.Price,
+        ProductBrand = product.ProductBrand.Name,
+        ProductType = product.ProductType.Name
+      }).ToList();
     }
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id)
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
-      return await _productRepo.GetEntityWithSpec(spec);
+      var product = await _productRepo.GetEntityWithSpec(spec);
+
+      return new ProductToReturnDto
+      {
+        Id = product.Id,
+        Name = product.Name,
+        Description = product.Description,
+        PictureUrl = product.PictureUrl,
+        Price = product.Price,
+        ProductBrand = product.ProductBrand.Name,
+        ProductType = product.ProductType.Name
+      };
     }
 
     [HttpGet("brands")]
