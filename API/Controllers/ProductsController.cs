43fbb9a236778d15aed7ed435b22511558c4056b
Alberto Biancardi <fieldyn@gmail.com>
2021-05-02 18:34:06 -0500

    43

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using System.Linq;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -16,10 +17,14 @@ namespace API.Controllers
     private readonly IGenericRepository<Product> _productRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<ProductType> _productTypeRepo;
+    private readonly IMapper _mapper;
+
     public ProductsController(IGenericRepository<Product> productRepo,
-      IGenericRepository<ProductBrand> productBrandRepo,
-      IGenericRepository<ProductType> productTypeRepo)
+                              IGenericRepository<ProductBrand> productBrandRepo,
+                              IGenericRepository<ProductType> productTypeRepo,
+                              IMapper mapper)
     {
+      _mapper = mapper;
       _productTypeRepo = productTypeRepo;
       _productBrandRepo = productBrandRepo;
       _productRepo = productRepo;
@@ -47,16 +52,7 @@ namespace API.Controllers
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
       var product = await _productRepo.GetEntityWithSpec(spec);
 
-      return new ProductToReturnDto
-      {
-        Id = product.Id,
-        Name = product.Name,
-        Description = product.Description,
-        PictureUrl = product.PictureUrl,
-        Price = product.Price,
-        ProductBrand = product.ProductBrand.Name,
-        ProductType = product.ProductType.Name
-      };
+      return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
