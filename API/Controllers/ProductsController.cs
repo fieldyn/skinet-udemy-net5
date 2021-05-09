7dc9764f173f9e718f3fecf9b32c137ab8e92d06
Alberto Biancardi <fieldyn@gmail.com>
2021-05-09 16:45:47 -0500

    56

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,6 +7,8 @@ using Core.Specifications;
 using API.Dtos;
 using System.Linq;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
@@ -36,10 +38,14 @@ namespace API.Controllers
       return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
       var product = await _productRepo.GetEntityWithSpec(spec);
+
+      if (product == null) return NotFound(new ApiResponse(404));
 
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
