c41cf3ea0f76a071de8cc3a24a25f246f7663147
Alberto Biancardi <fieldyn@gmail.com>
2021-05-09 16:03:23 -0500

    53

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -44,6 +45,23 @@ namespace API
             {
               c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+          var errors = actionContext.ModelState
+            .Where(e => e.Value.Errors.Count > 0)
+            .SelectMany(x => x.Value.Errors)
+            .Select(x => x.ErrorMessage).ToArray();
+
+          var errorResponse = new ApiValidationErrorResponse
+          {
+            Errors = errors
+          };
+          return new BadRequestObjectResult(errorResponse);
+        };
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
