b74212433b463bb501d2a989984e9488a8f4cd36
Alberto Biancardi <fieldyn@gmail.com>
2021-06-02 21:35:19 -0500

    88

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagination } from '../shared/models/pagination';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'http://localhost:5000/api/'
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IPagination> {
+    return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
+}
