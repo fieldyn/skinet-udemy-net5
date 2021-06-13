be6481faa3272d6990d4ed2dd4aead1ebe40cfc9
Alberto Biancardi <fieldyn@gmail.com>
2021-06-13 15:18:37 -0500

    101

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { FormsModule } from '@angular/forms';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -14,6 +17,7 @@ import { FormsModule } from '@angular/forms';
   exports: [
     PaginationModule,
     FormsModule,
+    PagingHeaderComponent
   ]
 })
 export class SharedModule { }
