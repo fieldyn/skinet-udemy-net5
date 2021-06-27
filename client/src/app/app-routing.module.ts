9772f47bfe462b218cf1695dbc05db34bce4675e
Alberto Biancardi <fieldyn@gmail.com>
2021-06-27 18:19:50 -0500

    146

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent , data: {breadcrumb: 'Home'}},
@@ -13,6 +11,7 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'} },
   { path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'} },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod=> mod.ShopModule), data: {breadcrumb: 'Shop'} },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod=> mod.BasketModule), data: {breadcrumb: 'Basket'} },
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ];
 
