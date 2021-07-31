6830519cebe49e804eefa74b2939944a2a8d8c74
Alberto Biancardi <fieldyn@gmail.com>
2021-07-31 14:11:32 -0500

    194

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -24,6 +24,24 @@ export class AccountService {
           this.currentUserSource.next(user);
         }
       }));
+  }
+
+  getCurrentUserValue() {
+    return this.currentUserSource.value;
+  }
+
+  loadCurrentUser(token: string) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<IUser>(`${this.baseUrl}account`, { headers }).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      }
+      ));
   }
 
   register(values: any) {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -6,13 +7,29 @@ import { BasketService } from './basket/basket.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Skinet';
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) {
 
   }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
@@ -22,4 +39,5 @@ export class AppComponent implements OnInit{
       });
     }
   }
+
 }
